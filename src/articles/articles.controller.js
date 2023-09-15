af6169f309aa6c81c55158272e8c58961718f8b5
aries grey <ariesgrey88@gmail.com>
2023-09-14 23:13:00 -0500

    implemented error logs for POST req to /articles

--- src/articles/articles.controller.js
+++ src/articles/articles.controller.js
@@ -2,21 +2,31 @@ const service = require("./articles.service");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 
 function hasData(req, res, next) {
-  if (req.body.data) {
-    return next();
-  }
-  next({ status: 400, message: "body must have data property" });
+	const methodName = "hasData";
+	req.log.debug({ __filename, methodName, body: req.body });
+	if (req.body.data) {
+		req.log.trace({ __filename, methodName, valid: true });
+		return next();
+	}
+	const message = "body must have data property";
+	next({ status: 400, message: message });
+	req.log.trace({ __filename, methodName, valid: false }, message);
 }
 
 function dataHas(propertyName) {
-  return (request, response, next) => {
-    const { data = {} } = request.body;
-    const value = data[propertyName];
-    if (value) {
-      return next();
-    }
-    next({ status: 400, message: `Article must include a ${propertyName}` });
-  };
+	const methodName = `dataHas('${propertyName}')`;
+	return (req, res, next) => {
+		req.log.debug({ __filename, methodName, body: req.body });
+		const { data = {} } = req.body;
+		const value = data[propertyName];
+		if (value) {
+			req.log.trace({ __filename, methodName, valid: true });
+			return next();
+		}
+		const message = `Article must include a ${propertyName}`;
+		next({ status: 400, message: message });
+		req.log.trace({ __filename, methodName, valid: false }, message);
+	};
 }
 
 const hasTitle = dataHas("title");
@@ -24,20 +34,23 @@ const hasUrl = dataHas("url");
 const hasSummary = dataHas("summary");
 
 async function create(req, res) {
-  const newObservation = await service.create(req.body.data);
-  res.status(201).json({
-    data: newObservation,
-  });
+	const newObservation = await service.create(req.body.data);
+	res.status(201).json({
+		data: newObservation,
+	});
 }
 
 async function list(req, res) {
-  const data = await service.list();
-  res.json({
-    data,
-  });
+	const methodName = "list";
+	req.log.debug({ __filename, methodName });
+	const data = await service.list();
+	res.json({
+		data,
+	});
+	req.log.trace({ __filename, methodName, return: true, data });
 }
 
 module.exports = {
-  create: [hasData, hasTitle, hasUrl, hasSummary, asyncErrorBoundary(create)],
-  list: asyncErrorBoundary(list),
+	create: [hasData, hasTitle, hasUrl, hasSummary, asyncErrorBoundary(create)],
+	list: asyncErrorBoundary(list),
 };
